79975e5eb91fa58588f9068e7408b261a5b3d9a3
yosef-ZzZ <yosef.zebene1@gmail.com>
2019-12-30 17:42:39 -0600

    Added level generator
    Added level generator and created a few more files for organization.

---
+++ Sword & Stick/Assets/Scripts/LevelGen/ColorToPrefab.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ColourToPrefab {
+    // Sets a colour to a prefab
+    public Color colour;
+    public GameObject prefab;
+}
---
+++ Sword & Stick/Assets/Scripts/LevelGen/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // Enter a draw map design (PNG or Photoshop file)
+    public Texture2D map;
+    // Substitutes selected colour with enterd prefab
+    public ColourToPrefab[] colourMapping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel () {
+        for (int x = 0; x < map.width; x++) {
+            for (int y = 0; y < map.height; y++) {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile (int x, int y) {
+        Color pixelColor = map.GetPixel(x, y);
+
+        // Ignore transparent pixel
+        if (pixelColor.a == 0) {
+            return;
+        }
+
+        foreach (ColourToPrefab colourMapping in colourMapping) {
+            // Check if the assigned colour is the same as the pixels colour
+            if (colourMapping.colour.Equals(pixelColor)) {
+                Vector2 position = new Vector2(x, y);
+                // Replace pixel with assigned prefab
+                Instantiate(colourMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+}
