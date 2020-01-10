b44b1664301d3528ad5b6e758206453fc633ad90
yosef-ZzZ <yosef.zebene1@gmail.com>
2020-01-09 18:22:02 -0600

    Player Control

---
+++ Sword & Stick/Assets/Scripts/Player/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damagetoplayer = 1;
+
+    // This fucntion checks what the enemy collided with and deals damage if it is the player
+   void OnTriggerEnter2D(Collider2D damageCollider) {
+       // This checks if the collider touched a player
+       if (damageCollider.CompareTag("Player")) {
+           // Player takes damage
+           damageCollider.GetComponent<PlayerHealth>().health -= damagetoplayer;
+
+           Debug.Log(damageCollider.GetComponent<PlayerHealth>().health);
+       }
+   }
+}
---
+++ Sword & Stick/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 5;
+
+    // Update is called once per frame
+    private void Update () {
+        if (health <= 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
