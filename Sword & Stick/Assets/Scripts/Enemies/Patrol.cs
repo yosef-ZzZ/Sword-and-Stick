f71cd4438dacb1f9e795ca3f72f6cc14851e8516
yosef-ZzZ <yosef.zebene1@gmail.com>
2020-01-13 18:55:03 -0600

    Added enemy damage

---
+++ Sword & Stick/Assets/Scripts/Enemies/Patrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+  public float speed;
+  public float distance;
+
+  private bool movingLeft = false;
+
+  public Transform groundDetection;
+
+  public Animator animator;                // Get Skeleton animator
+
+  void Update(){
+      transform.Translate(Vector2.right * speed * Time.deltaTime);
+      animator.SetFloat("Speed", Mathf.Abs(speed));
+
+      RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+      if(groundInfo.collider == false) {
+          if (movingLeft == true){
+              transform.eulerAngles = new Vector3(0, 0, 0);
+              movingLeft = false;
+          } else {
+              transform.eulerAngles = new Vector3(0, 180, 0);
+              movingLeft = true;
+          }
+      }
+  }
+}
