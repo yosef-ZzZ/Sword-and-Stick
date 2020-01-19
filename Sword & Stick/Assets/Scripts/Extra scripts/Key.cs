fb38f66af22e14a77b02efbfecc3f35d1dc2ba0f
yosef-ZzZ <yosef.zebene1@gmail.com>
2020-01-18 22:42:48 -0600

    Added door and key
    Still need sprites for these

---
+++ Sword & Stick/Assets/Scripts/Extra scripts/Key.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public virtual void OnTriggerEnter2D (Collider2D collider){
+        // Checks if the player touched the key
+        if (collider.CompareTag("Player")) {
+            // Sets the player has the key value to true
+            collider.GetComponent<Keymanager>().hasKey = true;
+            // Makes key not visible
+            gameObject.SetActive (false);
+        }
+       
+    }
+}
